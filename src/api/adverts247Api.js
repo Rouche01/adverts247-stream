61a732cf993f50765f4a7cfc052686d1da6ddf07
Richard Emate <richardemate@gmail.com>
2022-03-21 13:32:15 +0100

    update api url

--- src/api/adverts247Api.js
+++ src/api/adverts247Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 let instance = axios.create({
-    baseURL: `https://frozen-escarpment-07199.herokuapp.com/`
+    baseURL: `https://frozen-escarpment-07199.herokuapp.com/api/`
 })
 
 
--- src/context/StreamingContext.js
+++ src/context/StreamingContext.js
@@ -1,36 +1,37 @@
-import createDataContext from './createDataContext';
-import adverts247Api from '../api/adverts247Api';
-
+import createDataContext from "./createDataContext";
+import adverts247Api from "../api/adverts247Api";
 
 const streamingReducer = (state, action) => {
-    switch(action.type) {
-        case 'set_streaming_status':
-            return { ...state, streamingStatus: action.payload }
-        case 'set_error':
-            return { ...state, error: action.payload }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "set_streaming_status":
+      return { ...state, streamingStatus: action.payload };
+    case "set_error":
+      return { ...state, error: action.payload };
+    default:
+      return state;
+  }
+};
 
-
-const getStreamingStatus = dispatch => async(driverId, cancelTokn) => {
-
-    try {
-        const response = await adverts247Api.get(`/driver/${driverId}`, { cancelToken: cancelTokn });
-        // console.log(response.data.driver);
-        dispatch({ type: 'set_streaming_status', payload: response.data.driver.deviceStatus });
-    } catch(err) {
-        dispatch({
-            type: 'set_error',
-            payload: err.message
-        });
-    }
-}
-
+const getStreamingStatus = (dispatch) => async (driverId, cancelTokn) => {
+  try {
+    const response = await adverts247Api.get(`/stream/${driverId}`, {
+      cancelToken: cancelTokn,
+    });
+    // console.log(response.data.driver);
+    dispatch({
+      type: "set_streaming_status",
+      payload: response.data.data.streamStatus,
+    });
+  } catch (err) {
+    dispatch({
+      type: "set_error",
+      payload: err.message,
+    });
+  }
+};
 
 export const { Context, Provider } = createDataContext(
-    streamingReducer,
-    { getStreamingStatus },
-    { streamingStatus: "off", error: '' }
-)
+  streamingReducer,
+  { getStreamingStatus },
+  { streamingStatus: "off", error: "" }
+);
